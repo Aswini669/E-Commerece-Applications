c93669ae122f261951f4bfdaf4bd698e3ab0021c
Gyanaranjan622 <karan123@gmail.com>
2026-01-19 18:10:20 +0530

    Centralized Configuration Server

--- user-service/src/main/java/com/ecom/user/controller/UserController.java
+++ user-service/src/main/java/com/ecom/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.ecom.user.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +14,11 @@ public class UserController {
 	@Autowired
 	UsersService usersService;
 	
+	@PostMapping("createUser")
 	public String createUser(@RequestBody UsersRequest usersRequest) {
 		long userId = usersService.createUser(usersRequest);
 		return "your profile created successfully: " + userId;
 	}
+	
+	
 }
